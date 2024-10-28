0a1d643a4d28b1df24d03a0ea057594523b277e3
Cainã Costa <me@cfcosta.com>
2024-10-28 13:36:55 -0300

    chore: nix setup

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        inherit (pkgs)
+          makeRustPlatform
+          mkShell
+          rust-bin
+          ;
+
+        rust = rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        rustPlatform = makeRustPlatform {
+          rustc = rust;
+          cargo = rust;
+        };
+
+        packages.default = rustPlatform.buildRustPackage {
+          name = "plonky2-u32";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          doCheck = false;
+        };
+      in
+      {
+        inherit packages;
+
+        devShells.default = mkShell {
+          name = "plonky2-u32";
+
+          buildInputs = with pkgs; [
+            rust
+
+            cargo-nextest
+            cargo-watch
+          ];
+        };
+      }
+    );
+}
